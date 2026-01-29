adf97471c772f2b2fee7b6b8f5904bcc1ecf4068
Abhishek <abhishekjha96500@gmail.com>
2026-01-29 20:17:09 +0530

    fixed contact section.

--- src/app/api/send/route.js
+++ src/app/api/send/route.js
@@ -7,7 +7,7 @@ export async function POST(request) {
     if (!email || !subject || !message) {
       return NextResponse.json(
         { success: false, error: "Missing fields" },
-        { status: 400 }
+        { status: 400 },
       );
     }
 
@@ -15,14 +15,12 @@ export async function POST(request) {
 
     if (!GOOGLE_SCRIPT_URL) {
       console.error("Missing GOOGLE_SCRIPT_URL env variable");
-      return NextResponse.json(
-        { success: false },
-        { status: 500 }
-      );
+      return NextResponse.json({ success: false }, { status: 500 });
     }
 
     const res = await fetch(GOOGLE_SCRIPT_URL, {
       method: "POST",
+      redirect: "follow",
       headers: {
         "Content-Type": "application/json",
       },
@@ -40,9 +38,6 @@ export async function POST(request) {
     return NextResponse.json({ success: true });
   } catch (err) {
     console.error(err);
-    return NextResponse.json(
-      { success: false },
-      { status: 500 }
-    );
+    return NextResponse.json({ success: false }, { status: 500 });
   }
 }
